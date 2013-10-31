4b80ce8408c2dfb6f3f931833ebd95dd5deb7a2f
Pavol Rusnak <stick@gk2.sk>
2013-10-31 14:31:41 +0100

    secp256k1 is now part of ecdsa module

--- coinkit/address.py
+++ coinkit/address.py
@@ -4,18 +4,6 @@ import ecdsa
 from base58 import base58_check_decode, base58_check_encode, base58_encode_padded
 from hash import shash, rhash
 
-_p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2FL
-_r = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141L
-_b = 0x0000000000000000000000000000000000000000000000000000000000000007L
-_a = 0x0000000000000000000000000000000000000000000000000000000000000000L
-_Gx = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798L
-_Gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8L
-curve_secp256k1 = ecdsa.ellipticcurve.CurveFp( _p, _a, _b )
-generator_secp256k1 = ecdsa.ellipticcurve.Point( curve_secp256k1, _Gx, _Gy, _r )
-order = generator_secp256k1.order()
-oid_secp256k1 = (1,3,132,0,10)
-SECP256k1 = ecdsa.curves.Curve('SECP256k1', curve_secp256k1, generator_secp256k1, oid_secp256k1 )
-
 class Address(object):
 
     @classmethod
@@ -47,7 +35,7 @@ class Address(object):
         if not secret:
             secret = os.urandom(32)
         self.secret = ecdsa.util.string_to_number(secret)
-        self.pubkey = ecdsa.ecdsa.Public_key(generator_secp256k1, generator_secp256k1 * self.secret)
+        self.pubkey = ecdsa.ecdsa.Public_key(ecdsa.ecdsa.generator_secp256k1, ecdsa.ecdsa.generator_secp256k1 * self.secret)
         self.privkey = ecdsa.ecdsa.Private_key(self.pubkey, secret)
         pubhex = ('04' + '%064x' % self.pubkey.point.x() + '%064x' % self.pubkey.point.y()).decode('hex')
         self.pub = base58_check_encode(rhash(pubhex))
